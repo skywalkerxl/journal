e81f02d9c36c7bed2c46081ce6cdb2996617c701
skywalker <skywalker_x@163.com>
2017-07-13 23:29:06 +0800

    原型链的使用方法1、2

---
+++ 强大的原型和原型链/js/prototypeChainPart1.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Administrator on 2017/7/13.
+ */
+//学习了原型之后，可以通过如下方法来实现
+
+console.log('************************ 原型使用方法1 *************************');
+/* 在使用原型之前，我们先将代码进行修改，统一归到Calculator中 */
+var Calcultor = function (decimalDigits, tax) {
+    this.decimalDigits = decimalDigits;
+    this.tax = tax;
+};
+Calcultor.prototype = {
+    add : function (x, y) {
+        return x + y;
+    },
+    substract : function (x, y) {
+        return x - y;
+    }
+
+};
+console.log( Calcultor.add(10, 20) );
---
+++ 强大的原型和原型链/js/prototypeChainPart2.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Administrator on 2017/7/13.
+ */
+console.log('************************ 原型使用方法2 *************************');
+//第二种方式则是，在赋值原型prototype的时候使用function立即执行的表达式来赋值,即如下格式
+Calculator.prototype = function () {}();
+//这种做法的好处，就是可以封装私有的function，通过return的形式暴露出简单的使用名称，从而达到public/private的效果
+Calculator.prototype = function () {
+    add = function (x, y) {
+        return x + y;
+    },
+    substract = function(x, y){
+        return x - y;
+    }
+    return{
+        add :add,
+        subtract: subtract
+    }
+}();
